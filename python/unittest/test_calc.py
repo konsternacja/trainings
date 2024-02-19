d81da361bc1fea03606211dbb63c49584d7f7c79
Konstancja <galat.konstancja@gmail.com>
2024-02-19 08:43:20 +0100

    unittest

---
+++ python/unittest/test_calc.py
@@ -0,0 +1,38 @@
+import unittest
+import calc
+
+
+class TestCalc(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(calc.add(5,1), 6)
+        self.assertEqual(calc.add(5,-1), 4)
+        self.assertEqual(calc.add(-1,-1), -2)
+        
+        
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(5,1), 4)
+        self.assertEqual(calc.subtract(5,-1), 6)
+        self.assertEqual(calc.subtract(-1,-1), 0)
+        
+        
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(5,1), 5)
+        self.assertEqual(calc.multiply(5,-1), -5)
+        self.assertEqual(calc.multiply(-1,-1), 1)
+        
+        
+    def test_divide(self):
+        self.assertEqual(calc.divide(5,1), 5)
+        self.assertEqual(calc.divide(5,-1), -5)
+        self.assertEqual(calc.divide(-1,-1), 1)
+        self.assertEqual(calc.divide(5, 2), 2.5)
+
+        # context manager
+        with self.assertRaises(ValueError):
+            calc.divide(10,0)
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
